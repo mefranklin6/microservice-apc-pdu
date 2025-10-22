e1e10e86b01782c76ec4c342c02d7b61a1120b98
mefranklin6 <mefranklin@csuchico.edu>
2025-10-22 09:45:31 -0700

    initial commit

--- source/microservice.go
+++ source/microservice.go
@@ -1,1 +1,70 @@
 package main
+
+import (
+	"errors"
+
+	"github.com/mefranklin6/microservice-framework/framework"
+)
+
+func setFrameworkGlobals() {
+	framework.MicroserviceName = "OpenAV APC PDU Microservice"
+	framework.DefaultSocketPort = 23
+	framework.KeepAlive = true
+	framework.RegisterMainGetFunc(doDeviceSpecificGet)
+	framework.RegisterMainSetFunc(doDeviceSpecificSet)
+}
+
+// Every microservice using this golang microservice framework needs to provide this function to invoke functions to do sets.
+// socketKey is the network connection for the framework to use to communicate with the device.
+// setting is the first parameter in the URI.
+// arg1 are the second and third parameters in the URI.
+//
+//	  Example PUT URIs that will result in this function being invoked:
+//		 ":address/:setting/"
+//	  ":address/:setting/:arg1"
+//	  ":address/:setting/:arg1/:arg2"
+func doDeviceSpecificSet(socketKey string, setting string, arg1 string, arg2 string, arg3 string) (string, error) {
+	function := "doDeviceSpecificSet"
+
+	// Add a case statement for each set function your microservice implements.  These calls can use 0, 1, or 2 arguments.
+	switch setting {
+	//case "outlet":
+	//		return setOutlet(socketKey, arg1, arg2) // arg1 = outlet number, arg2 = state
+	}
+
+	// If we get here, we didn't recognize the setting.  Send an error back to the config writer who had a bad URL.
+	errMsg := function + " - unrecognized setting in URI: " + setting
+	framework.AddToErrors(socketKey, errMsg)
+	err := errors.New(errMsg)
+	return setting, err
+}
+
+// Every microservice using this golang microservice framework needs to provide this function to invoke functions to do gets.
+// socketKey is the network connection for the framework to use to communicate with the device.
+// setting is the first parameter in the URI.
+// arg1 are the second and third parameters in the URI.
+//
+//	  Example GET URIs that will result in this function being invoked:
+//		 ":address/:setting/"
+//	  ":address/:setting/:arg1"
+//	  ":address/:setting/:arg1/:arg2"
+func doDeviceSpecificGet(socketKey string, setting string, arg1 string, arg2 string) (string, error) {
+	function := "doDeviceSpecificGet"
+
+	switch setting {
+	case "outlet":
+		value, err := getOutlet(socketKey, arg1) // arg1 = outlet number
+		return value, err
+	}
+
+	// If we get here, we didn't recognize the setting.  Send an error back to the config writer who had a bad URL.
+	errMsg := function + " - unrecognized setting in URI: " + setting
+	framework.AddToErrors(socketKey, errMsg)
+	err := errors.New(errMsg)
+	return setting, err
+}
+
+func main() {
+	setFrameworkGlobals()
+	framework.Startup()
+}
