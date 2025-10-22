0984043c93cc28b009f5090a6eebf468cb8cfe85
mefranklin6 <mefranklin@csuchico.edu>
2025-10-22 09:45:44 -0700

    initial commit

--- source/driver.go
+++ source/driver.go
@@ -1,1 +1,15 @@
 package main
+
+import "github.com/mefranklin6/microservice-framework/framework"
+
+///////////////////////////////////////////////////////////////////////////////
+// Main functions //
+///////////////////////////////////////////////////////////////////////////////
+
+// Get functions //
+
+func getOutlet(socketKey string, outlet string) (string, error) {
+	//function := "getOutlet"
+	framework.Log(socketKey, "HelloWorld!")
+	return "", nil
+}
